396237264a72522eea8c0f1d731a98d47d22b1d0
awkwardartist <slider4655@gmail.com>
2022-04-12 19:17:50 -0400

    init

--- build.sh
+++ build.sh
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-nasm -f elf bootloader.asm -o bootloader.o
-g++ -w -fpermissive -m32 -fno-stack-protector -c kernel.cpp -o cppkernel.o
-nasm -f elf includes/realmode.asm -o realmode.o
-# experimental file (for opcodes)
-nasm -f bin experimentFile.asm -o exp.bin
-
-ld -m elf_i386 -T link.ld -o kernel.bin bootloader.o realmode.o cppkernel.o 
-grub-mkrescue -o AwkieOS.iso ./
-qemu-system-i386 -kernel ./kernel.bin
-
-# cleanup :)
-rm -r *.o
-rm -r *.bin
-rm -r *.out
-=======
 echo -e "compiling c modules..."
 # compile C files
 for i in lib/*.c lib/*.cpp ; do
@@ -77,4 +61,3 @@ fi
 
 qemu-system-i386 -kernel out/Kernel
 
->>>>>>> 2033a6f (init)
